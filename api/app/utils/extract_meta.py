6413b7b5c51782f4c637fa24ffe25a1f4f0ea5fd
atsushiflee <atsushiflee@icloud.com>
2020-07-31 15:48:20 -0700

    Add comments

--- api/app/utils/extract_meta.py
+++ api/app/utils/extract_meta.py
@@ -3,12 +3,23 @@ import cv2
 
 
 class ExtractMetaData():
+    """
+    Extract metadata from uploaded video.
+    """
 
     def __init__(self, video, cap):
+        """
+        Constructor
+        :param video: uploaded video
+        :param cap: uploaded video as caption object
+        """
         self.video = video
         self.cap = cap
 
     def create_meta_data(self):
+        """
+        Collect all of result from each functions
+        """
         filename = self.extract_filename()
         create_time = self.create_timestamp()
         mime_type = self.extract_mimetype()
@@ -27,18 +38,30 @@ class ExtractMetaData():
         return meta_data
 
     def extract_filename(self):
+        """
+        Extract filename
+        """
         filename = self.video.filename
         return filename
 
     def create_timestamp(self):
+        """
+        Create timestamp
+        """
         create_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         return create_time
 
     def extract_mimetype(self):
+        """
+        Extract minetype
+        """
         mime_type = self.video.mimetype
         return mime_type
 
     def extract_duration(self):
+        """
+        Extract duration
+        """
         fps = self.cap.get(cv2.CAP_PROP_FPS)
         frame = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         duration = frame / fps
@@ -46,6 +69,9 @@ class ExtractMetaData():
         return f"{duration} s"
 
     def extract_width_and_height(self):
+        """
+        Extract width and height of frames
+        """
 
         width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
